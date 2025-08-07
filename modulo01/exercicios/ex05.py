f044fd5c2284bc6adfddb230536625c0b3efa089
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-07 09:37:11 -0300

    Feito o quarto exercício.

---
+++ modulo01/exercicios/ex05.py
@@ -0,0 +1,7 @@
+# Escreva um programa Java que converta graus Celsius para Fahrenheit.
+
+celsius = float(input("Informe o valor da temperatura atual: "))
+
+fahrenheit = (celsius / 1.8) + 32
+
+print(f'A temperatura {celsius}ºC é equivalente a {fahrenheit}ºF')
