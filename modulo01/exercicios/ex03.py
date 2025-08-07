9463911947b3cc9d14289434540d3b8db0184e4d
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-07 09:30:40 -0300

    Feito o terceiro exercício.

---
+++ modulo01/exercicios/ex03.py
@@ -0,0 +1,9 @@
+# Escreva um programa Java que calcule a média de três números inteiros.
+
+nota_01 = int(input('Informe a sua primeira nota: '))
+nota_02 = int(input('Informe a sua segunda nota: '))
+nota_03 = int(input('Informe a sua terceira nota: '))
+
+media = (nota_01 + nota_02 + nota_03) / 3
+
+print(f'A média do aluno é {media}.')
