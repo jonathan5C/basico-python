502e441c592d0195dedda7a9c473d3e135735708
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2025-08-07 09:33:18 -0300

    Feito o quarto exercício.

---
+++ modulo01/exercicios/ex04.py
@@ -0,0 +1,10 @@
+# Escreva um programa que determine se um número é positivo, negativo ou zero.
+
+numero = int(input('Informe um número: '))
+
+if numero > 0:
+    print('Número positivo')
+elif numero < 0:
+    print('Número negativo')
+else:
+    print('Número é zero.')
